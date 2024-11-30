a26d285a755e498f31ef1d5e05b0a63dcd55f613
db-one. <20243226+db-one@users.noreply.github.com>
2024-11-30 11:06:13 +0800

    更新

--- build/immortalwrt_x86_64/custom.sh
+++ build/immortalwrt_x86_64/custom.sh
@@ -5,7 +5,7 @@
 # sudo -E apt-get -y install rename
 
 # 更新feeds文件
-src-git dbone https://github.com/db-one/dbone-packages.git;23.05
+echo "src-git dbone-packages https://github.com/db-one/dbone-packages.git;23.05" feeds.conf.default
 cat feeds.conf.default
 
 # 更新并安装源
